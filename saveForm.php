183ca73d08b5e2314809185d7ae06e80ab7b3b14
Wietse Neven <info@wn-multimedia.nl>
2015-11-28 20:10:41 +0100

    init

--- saveForm.php
+++ saveForm.php
@@ -5,6 +5,7 @@ kirbytext::$tags['saveForm'] = array(
 		'fields'
 	),
 	'html' => function($tag) {
+		// If form was submitted go proces to form, else render the form
 		if ($_POST["submitted"] == 'true') {
 			require_once('procesForm.php');
 		} else {
