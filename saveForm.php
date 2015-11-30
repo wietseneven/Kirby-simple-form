580fab3d13c4ab027ed046ee2e8fd21ff112cd26
Wietse Neven <info@wn-multimedia.nl>
2015-11-30 13:35:08 +0100

    Functionality to add custom confirmation

--- saveForm.php
+++ saveForm.php
@@ -2,14 +2,26 @@
 
 kirbytext::$tags['saveForm'] = array(
 	'attr' => array(
-		'fields'
+		'fields',
+		'confirmation'
 	),
 	'html' => function($tag) {
 		// If form was submitted go proces to form, else render the form
 		if ($_POST["submitted"] == 'true') {
-			require_once('lib/procesForm.php');
+			require_once(__DIR__ . DS . 'lib' . DS . 'procesForm.php');
+			// Clear post data to prevent double submissions
+			header('Location:'.$_SERVER['PHP_SELF'].'?formres=true');
+		} else if ($_GET['formres'] == 'true') {
+			if ($tag->attr('confirmation')) {
+				$confirmationMessage = $tag->attr('confirmation');
+			} else {
+				$confirmationMessage = 'Form submitted succesfully';
+			}
+			return $confirmationMessage;
 		} else {
-			require_once('lib/createForm.php');
+			require_once(__DIR__ . DS . 'lib' . DS . 'createForm.php');
+			return $form;
 		}
+
 	}
 );
